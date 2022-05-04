8d65eea5e6ea785c2e352c331c94f2c4bf650cca
Anurag Alakkat <anurag.alakkat@goavega.com>
2022-05-04 11:40:30 +0530

    controllers

---
+++ src/student/student.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Put } from "@nestjs/common";
+
+@Controller('students')
+export class StudentController {
+    @Get()
+    getStudents() {
+        return "All students"
+    }
+
+    @Get('/:studentId')
+    getStudentByID() {
+        return "student by id"
+    }
+
+    @Post()
+    createStudent() {
+        return "create a student"
+    }
+
+    @Put('/:studentId')
+    updateStudent() {
+        return "update a student"
+    }
+}
